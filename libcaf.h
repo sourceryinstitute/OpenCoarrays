bafe901feff89515900b26068aaaf50fd2844d55
Tobias Burnus <burnus@net-b.de>
2014-04-15 00:47:29 +0200

    Add stup implementation for co_min/max/sum
    * dev/co_collectives.f90: New.
    * dev/libcaf.h (co_sum, co_min, co_max): Add prototypes.
    * dev/single/single.c (co_sum, co_min, co_max): Implement it.

--- libcaf.h
+++ libcaf.h
@@ -136,6 +136,13 @@ void PREFIX(send_desc) (caf_token_t, size_t, int, gfc_descriptor_t*,
 void PREFIX(send_desc_scalar) (caf_token_t, size_t, int, gfc_descriptor_t*,
 			       void*, bool);
 
+void PREFIX (co_max) (gfc_descriptor_t *, gfc_descriptor_t *, int, int *,
+		      char *, int);
+void PREFIX (co_min) (gfc_descriptor_t *, gfc_descriptor_t *, int, int *,
+		      char *, int);
+void PREFIX (co_sum) (gfc_descriptor_t *, gfc_descriptor_t *, int, int *,
+		      char *, int);
+
 void PREFIX(sync_all) (int *, char *, int);
 void PREFIX(sync_images) (int, int[], int *, char *, int);
 
