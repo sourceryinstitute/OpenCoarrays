8b335855c31cf4cfb93a52e868cad6d7c118ced5
Alessandro Fanfarillo <fanfarillo@ing.uniroma2.it>
2014-11-18 16:08:45 +0100

    New sameloc test case
    Signed-off-by: Alessandro Fanfarillo <fanfarillo@ing.uniroma2.it>

--- tests/src/send-get/sameloc.f90
+++ tests/src/send-get/sameloc.f90
@@ -1,9 +1,13 @@
+! In order to run this test successfully the efficient
+! strided transfer support must be disabled.
 program sameloc
   implicit none
 
   integer,codimension[*] :: a
   integer,dimension(10),codimension[*] :: b,c
+  integer,dimension(9,10),codimension[*] :: m
   integer,dimension(10) :: t
+  integer :: i,j
   
   a = 10
   b(1:5) = 1
@@ -14,10 +18,46 @@ program sameloc
   t(:) = b(:)
   t(1:5) = b(2:6)
 
+  do i=1,9
+    m(i,:) = (/ (j, j = 1, 10) /)
+  enddo
+
   sync all
 
   a = a[1]
-  if (this_image() == 1) write(*,*) 'OK'
+  if (this_image() == 1) write(*,*) 'OK',a
+
+  t = (/ (j, j = 1, 10) /)
+
+  if(this_image() == 1) then
+    c = m(1,:)[1]
+    if(any(c(:) /= t(:))) then 
+      call abort()
+    else
+      write(*,*) 'ok get row'
+    endif    
+  endif
+
+  sync all
+
+  if(this_image() == 1) then
+    do i=1,10
+      if(m(9,i)[1] /= t(i)) then
+	write(*,*) 'pos',i,'value get',m(9,i)[1],'value t',t(i)
+!	call abort()
+      else
+	write(*,*) 'Ok get element from matrix'
+      endif
+    enddo
+  endif
+
+  t(:) = b(:)
+  t(1:5) = b(2:6)
+
+  c(1:5) = 1
+  c(6:10) = -1
+
+  sync all
 
   if(this_image() == 1) then
     b(1:5)[1] = b(2:6)
@@ -50,6 +90,7 @@ program sameloc
   if(this_image() == 1) then 
     c(10:1:-1)[1] = c(:)
     if(any(t(:) /= c(:))) then
+      write(*,*) 'Error in put reversed'
       write(*,*) c
       write(*,*) t
       call abort()
