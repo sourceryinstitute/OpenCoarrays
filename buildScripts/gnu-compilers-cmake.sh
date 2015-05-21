30a10e52478a8f17e1fd048866dfad62e45c749e
Damian Rouson <damian@rouson.net>
2015-05-21 11:57:29 -0700

    Manually redoing May 20 commits to bitbucket:
        Added LEGACY_ARCHITECTURE argument to the GNU build script to switch between libfft_sse.a and libfft_avx.a.  The latter improves performan
    
        Set up for execution of all integration and unit tests via CTest.   (Performance tests must be run manually.)
        6 of 12 tests pass.  All remaining tests fail only because they do not print the expected "Test passed" string.
        Code that verifies correct execution must be added along with conditional printing of the aforementioned string.
    
        Completed set-up of CMake infrustructure for building libcaf_mpi, libcaf_single, and all tests.  Testing is now enabled, but the test resu
    
        Updated CMake build script to reflect new directory structure and to install in the build directory under lib/cmake/opencoarrays.

--- buildScripts/gnu-compilers-cmake.sh
+++ buildScripts/gnu-compilers-cmake.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
-SOURCE_PATH=../opencoarrays
+SOURCE_PATH=../src
 EXTRA_ARGS=$@
 
 rm -rf CMakeCache.txt CMakeFiles
 cmake \
+  -D CMAKE_Fortran_COMPILER:FILEPATH=mpif90 \
+  -D CMAKE_Fortran_FLAGS:STRING="-fcoarray=lib -Wall -fcheck=all -Ofast " \
   -D CMAKE_BUILD_TYPE:STRING=DEBUG \
   -D CMAKE_C_COMPILER:FILEPATH=gcc \
   -D CMAKE_VERBOSE_MAKEFILE:BOOL=TRUE \
-  -D CMAKE_INSTALL_PREFIX:PATH=${PWD}/lib \
+  -D LEGACY_ARCHITECTURE:BOOL=TRUE \
+  -D CMAKE_INSTALL_PREFIX:PATH=${PWD} \
+  -D CMAKE_TOOLCHAIN_FILE:FILEPATH=../toolchain.cmake \
 $EXTRA_ARGS \
 $SOURCE_PATH
+
+# Edit the library path below and insert this line above if the LD_LIBRARY_PATH environment variable is not set:
+#  -D CMAKE_Fortran_FLAGS:STRING="-L/opt/opencoarrays/lib -fcoarray=lib -Wall -fcheck=all -Ofast " \
