de6f50e474fc0dfe11019eaf16d2e42e09fcd23a
Damian Rouson <damian@rouson.net>
2015-05-22 11:54:58 -0700

    Updated the INSTALL file to include instructions for building and running the OpenCoarrays tests wiht CMake.  Added a LICENSE soft-link to the COPYRIGHT file.  Added comments to buildScripts/gnu-compilers-cmake.sh.

--- buildScripts/gnu-compilers-cmake.sh
+++ buildScripts/gnu-compilers-cmake.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
+# Specify the path to OpenCoarrays source tree (be sure your build directory is not 
+# inside the specific path):
 SOURCE_PATH=../src
 EXTRA_ARGS=$@
+
+# If the LD_LIBRARY_PATH environment variable is not set on the platform where you 
+# are building, either set it to the location of libcaf_mpi.a on your system or prepend 
+# the path to the  quoted text in the CMAKE_Fortran_FLAGS argument below. For example,
+# -D CMAKE_Fortran_FLAGS:STRING="-L/opt/opencoarrays/lib -fcoarray=lib -Wall -fcheck=all -Ofast " \
+
+# The LEGACY_ARCHITECTURE argument is used only in the test suite to determine whether
+# to link to an FFT library with the older Intel SSE instructions or newer AVX instructions.
+
+# Edit the CMAKE_INSTALL_PREFIX argument to the installation destination of your choice.
+# The default used below is the present working directory.
 
 rm -rf CMakeCache.txt CMakeFiles
 cmake \
@@ -11,9 +24,6 @@ cmake \
   -D CMAKE_VERBOSE_MAKEFILE:BOOL=TRUE \
   -D LEGACY_ARCHITECTURE:BOOL=TRUE \
   -D CMAKE_INSTALL_PREFIX:PATH=${PWD} \
-  -D CMAKE_TOOLCHAIN_FILE:FILEPATH=../toolchain.cmake \
+  -D CMAKE_TOOLCHAIN_FILE:FILEPATH=${SOURCE_PATH}/../toolchain.cmake \
 $EXTRA_ARGS \
 $SOURCE_PATH
-
-# Edit the library path below and insert this line above if the LD_LIBRARY_PATH environment variable is not set:
-#  -D CMAKE_Fortran_FLAGS:STRING="-L/opt/opencoarrays/lib -fcoarray=lib -Wall -fcheck=all -Ofast " \
