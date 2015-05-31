6115cf75055c311bc3fde3302571af0a9fcbc5d3
Damian Rouson <damian@rouson.net>
2015-05-31 16:23:52 -0700

    Manually applied git patch from commit e4657ce8e39ed9deee2d2918f1d92c76e24afa0, which was committed on OS X.  Updatd CMake files to build the Navier-Stokes solver test on Linux with the SSE-enabled FFT library as the default.

--- buildScripts/linux-toolchain.cmake
+++
@@ -1,5 +0,0 @@
-set(HIGH_RESOLUTION_TIMER TRUE)
-set(CMAKE_Fortran_FLAGS "-std=f2008 -fcoarray=lib -Wall -fcheck=all -Ofast -Wintrinsics-std")
-set(CMAKE_SYSTEM_NAME Linux)
-include(CMakeForceCompiler)
-CMAKE_FORCE_Fortran_COMPILER(/opt/mpich/3.1.2/gnu/5.0/bin/mpif90 GNU)
