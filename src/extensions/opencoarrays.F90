f02ea134fe6e420476ab9fa74c588299b6df0346
Damian Rouson <damian@rouson.net>
2015-06-11 23:26:49 -0700

    Added wrappers for several Fortran 2008/2015 features for use with compilers that provide limited or no coarray support. The features include sync_all, this_image, num_images, error_stop, and co_sum, each of which wraps a like-named Fortran 2008 or proposed Fortran 2015 statement or procedure.  Also added a test of these features.  All tests pass on OS X (still need to add an appropriate check and output for the Navier-Stokes solver test on Linux).

--- src/extensions/opencoarrays.F90
+++ src/extensions/opencoarrays.F90
@@ -29,11 +29,15 @@ module opencoarrays
 #ifdef COMPILER_SUPPORTS_ATOMICS
   use iso_fortran_env, only : atomic_int_kind
 #endif
-  use iso_c_binding, only : c_int,c_char,c_ptr,c_loc,c_long
+  use iso_c_binding, only : c_int,c_char,c_ptr,c_loc,c_long,c_int32_t
   implicit none
 
   private
   public :: co_sum
+  public :: this_image
+  public :: num_images
+  public :: error_stop
+  public :: sync_all
 #ifdef COMPILER_SUPPORTS_ATOMICS
   public :: event_type
   public :: event_post
@@ -44,15 +48,15 @@ module opencoarrays
   end type
 #endif
 
+  ! Generic interface to co_sum with implementations for various types, kinds, and ranks
   interface co_sum
      module procedure co_sum_integer_scalar
-  end interface co_sum
+  end interface 
 
   ! Bindings for OpenCoarrays C procedures
   interface 
-    ! From ../libcaf.h 
-    !
-    ! C function prototype:
+
+    ! C function prototype from ../libcaf.h:
     ! void
     ! PREFIX (co_sum) (gfc_descriptor_t *a, int result_image, int *stat, char *errmsg,
     !                  int errmsg_len)
@@ -64,9 +68,41 @@ module opencoarrays
       character(len=1,kind=c_char), intent(out), optional :: errmsg
       integer(c_int), intent(in), value :: errmsg_len
     end subroutine
+
+    ! C function prototype from ../libcaf.h:
+    ! int PREFIX (this_image) (int);
+    function caf_this_image(coarray) bind(C,name="_gfortran_caf_this_image") result(image_num)
+      import :: c_int
+      integer(c_int), value, intent(in) :: coarray
+      integer(c_int)  :: image_num
+    end function
+
+    ! C function prototype from ../libcaf.h:
+    ! int PREFIX (num_images) (int, int);
+    function caf_num_images(coarray,dim_) bind(C,name="_gfortran_caf_num_images") result(num_images_)
+      import :: c_int
+      integer(c_int), value, intent(in) :: coarray,dim_
+      integer(c_int) :: num_images_
+    end function
+
+    ! C function prototype from ../libcaf.h
+    ! void PREFIX (error_stop) (int32_t) __attribute__ ((noreturn));
+    subroutine caf_error_stop(stop_code) bind(C,name="_gfortran_caf_error_stop") 
+      import :: c_int32_t
+      integer(c_int32_t), value, intent(in) :: stop_code
+    end subroutine 
+
+    ! C function prototype from ../libcaf.h
+    ! void PREFIX (sync_all) (int *, char *, int);
+    subroutine caf_sync_all(stat,errmsg,unused) bind(C,name="_gfortran_caf_sync_all") 
+      import :: c_int,c_char
+      integer(c_int), intent(out) :: stat,unused
+      character(c_char), intent(out) :: errmsg
+    end subroutine 
+
   end interface
 
-  ! From ../libcaf-gfortran-descriptor.h 
+  ! Enumeration from ../libcaf-gfortran-descriptor.h:
   !  enum
   !{ BT_UNKNOWN = 0, BT_INTEGER, BT_LOGICAL, BT_REAL, BT_COMPLEX,
   !  BT_DERIVED, BT_CHARACTER, BT_CLASS, BT_PROCEDURE, BT_HOLLERITH, BT_VOID,
@@ -80,7 +116,7 @@ module opencoarrays
      BT_ASSUMED
   end enum
 
-  ! From ../libcaf-gfortran-descriptor.h 
+  ! Type definition from ../libcaf-gfortran-descriptor.h:
   !typedef struct descriptor_dimension
   !{
   !  ptrdiff_t _stride;
@@ -91,13 +127,14 @@ module opencoarrays
 
   integer, parameter :: ptrdiff_t=c_long
 
+  ! Fortran derived type interoperable with like-named C type:
   type, bind(C) :: descriptor_dimension
      integer(ptrdiff_t) :: stride 
      integer(ptrdiff_t) :: lower_bound 
      integer(ptrdiff_t) :: ubound_ 
   end type
 
-  ! From ../libcaf-gfortran-descriptor.h 
+  ! Type definition from ../libcaf-gfortran-descriptor.h:
   !typedef struct gfc_descriptor_t {
   !  void *base_addr;
   !  size_t offset;
@@ -107,6 +144,7 @@ module opencoarrays
 
   integer, parameter :: max_dimensions=15
 
+  ! Fortran derived type interoperable with like-named C type:
   type, bind(C) :: gfc_descriptor_t  
     type(c_ptr) :: base_addr
     integer(ptrdiff_t) :: offset
@@ -117,7 +155,7 @@ module opencoarrays
 
 contains
 
-  ! Proposed Fortran 2015 co_sum parallel collective sum reduction
+  ! Proposed Fortran 2015 parallel collective sum reduction for integers of interoperable kind c_int
   subroutine co_sum_integer_scalar(a,result_image,stat,errmsg)
     implicit none
     integer(c_int), intent(inout), volatile, target, contiguous :: a(..)
@@ -148,14 +186,41 @@ contains
     
   end subroutine
 
+  ! Return the image number (MPI rank + 1)
+  function this_image()  result(image_num)
+    integer(c_int) :: image_num,unused
+    image_num = caf_this_image(unused)
+  end function
+
+  ! Return the total number of images
+  function num_images()  result(num_images_)
+    integer(c_int) :: num_images_,unused_coarray,unused_scalar
+    num_images_ = caf_num_images(unused_coarray,unused_scalar)
+  end function
+
+  ! Halt the execution of all images
+  subroutine error_stop(stop_code)
+    integer(c_int32_t), intent(in), optional :: stop_code
+    integer(c_int32_t), parameter :: default_code=-1_c_int32_t
+    integer(c_int32_t) :: code
+    code = merge(stop_code,default_code,present(stop_code))
+    call caf_error_stop(code)
+  end subroutine 
+
+  ! Impose a global execution barrier
+  subroutine sync_all(stat,errmsg,unused)
+    integer(c_int), intent(out), optional :: stat,unused
+    character(c_char), intent(out), optional :: errmsg
+    call caf_sync_all(stat,errmsg,unused)
+  end subroutine 
+
 #ifdef COMPILER_SUPPORTS_ATOMICS
-   !Proposed Fortran 2015 event_post procedure
-    subroutine event_post(this)
-      class(event_type), intent(inout) ::  this
-      if (.not.allocated(this%atom)) this%atom=0
-      call atomic_define ( this%atom, this%atom + 1_atomic_int_kind )
-    end subroutine
+   ! Proposed Fortran 2015 event_post procedure
+   subroutine event_post(this)
+     class(event_type), intent(inout) ::  this
+     if (.not.allocated(this%atom)) this%atom=0
+     call atomic_define ( this%atom, this%atom + 1_atomic_int_kind )
+   end subroutine
 #endif 
 
 end module
-
---
+++ src/tests/integration/extensions/tally_image_numbers.f90
@@ -0,0 +1,24 @@
+! Compute a parallel collective sum of all image numbers using the
+! "opencoarrays" Fortran module that is intended to support non-coarray 
+! Fortran compilers.  
+program main
+  use iso_fortran_env, only : error_unit
+  use iso_c_binding, only : c_int ,c_int32_t
+  use opencoarrays, only : this_image,num_images,co_sum,error_stop,sync_all
+  implicit none    
+  integer(c_int) :: me,image
+  me=this_image()
+  call co_sum(me)
+  call sync_all
+  ! Verify co_sum by comparing to the expected result:
+  if (me/=sum([(image,image=1,num_images())])) then
+    ! Report incorrect result:
+    write(error_unit,"(2(a,i2))") "Incorrect sum ",me," on image ",this_image()
+    ! Halt all images:
+    call error_stop
+  end if
+  ! Wait for every image to pass the test:
+  call sync_all
+  ! Image 1 reports success:
+  if (this_image()==1) print *, "Test passed."
+end program
