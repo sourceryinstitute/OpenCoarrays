1c8992dfb9b4d88ee8d3b70ca33f00648ef1ea18
Alessandro Fanfarillo <fanfarillo@ing.uniroma2.it>
2015-06-13 18:12:25 +0200

    Smaller test case for strided transfers
    Signed-off-by: Alessandro Fanfarillo <fanfarillo@ing.uniroma2.it>

--- src/tests/unit/send-get/strided_get.f90
+++ src/tests/unit/send-get/strided_get.f90
@@ -2,20 +2,22 @@ program strided_get
   implicit none
 
   integer :: i,me,np
-  integer,allocatable :: a(:,:,:,:)[:],b(:,:,:,:)
+  integer,allocatable :: a(:,:,:)[:],b(:,:,:)
 
   me = this_image()
   np = num_images()
 
-  allocate(a(0:11,-10:-5,-1:0,-1:5)[*],b(6,5,2,6))
+!  allocate(a(0:11,-10:-5,-1:0,-1:5)[*],b(6,5,2,6))
+  allocate(a(0:2,-1:1,1:2)[*],b(3,3,2))
 
   a = me
+  b = me
 
   sync all
 
   if(me > 1) then
-    b(1:6,:,:,:) = a(0:5,:,:,:)[me-1]
-    write(*,*) b(1:2,:,:,:)
+    b(1:2,:,:) = a(0:1,:,:)[me-1]
+    write(*,*) b(1:2,:,:)
   endif
 
 end program
