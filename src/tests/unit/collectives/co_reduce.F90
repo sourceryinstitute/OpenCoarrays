192ec709f136a60c100645b44e276457498c8442
Damian Rouson <damian@rouson.net>
2015-07-20 22:30:01 -0700

    Correcting some co_reduce problems and enabling the corresonding tests with GCC 5.2.1 and 6.0.0.  Disabling the corresponding code and test for GCC 4.9.  All tests pass.

--- src/tests/unit/collectives/co_reduce.F90
+++ src/tests/unit/collectives/co_reduce.F90
@@ -25,50 +25,107 @@
 ! (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 ! SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-module co_all_module
+module co_intrinsics_module
+#ifdef USE_EXTENSIONS
+  use opencoarrays
+#endif
   implicit none
+
   private
   public :: co_all
+  public :: co_product
+
+  interface co_all
+    module procedure co_all_c_bool
+  end interface
+
+  interface co_product
+    module procedure co_product_c_int,co_product_c_double
+  end interface
+
 contains
-  subroutine co_all(boolean)
-    logical, intent(inout) :: boolean
-    call co_reduce(boolean,both)
+
+  subroutine co_all_c_bool(a)
+    use iso_c_binding, only : c_bool
+    logical(c_bool), intent(inout) :: a
+    call co_reduce(a,and)
   contains
-    pure function both(lhs,rhs) result(lhs_and_rhs)
-      logical, intent(in) :: lhs,rhs
-      logical :: lhs_and_rhs
+    pure function and(lhs,rhs) result(lhs_and_rhs) bind(C,name="and")
+      logical(c_bool), intent(in) :: lhs,rhs
+      logical(c_bool) :: lhs_and_rhs
       lhs_and_rhs = lhs .and. rhs 
     end function
   end subroutine
+
+  subroutine co_product_c_int(a)  
+    use iso_c_binding, only : c_int
+    integer(c_int), intent(inout) :: a
+    call co_reduce(a,product_)
+  contains
+    pure function product_(lhs,rhs) result(lhs_x_rhs) bind(C,name="product_")
+      integer(c_int), intent(in) :: lhs,rhs
+      integer(c_int) :: lhs_x_rhs
+      lhs_x_rhs = lhs * rhs 
+    end function
+  end subroutine
+
+  subroutine co_product_c_double(a)  
+    use iso_c_binding, only : c_double
+    real(c_double), intent(inout) :: a
+    call co_reduce(a,product_)
+  contains
+    pure function product_(lhs,rhs) result(lhs_x_rhs)
+      real(c_double), intent(in) :: lhs,rhs
+      real(c_double) :: lhs_x_rhs
+      lhs_x_rhs = lhs * rhs 
+    end function
+  end subroutine
+
 end module 
 
 program main
   use iso_fortran_env, only : error_unit 
-  use iso_c_binding, only : c_int,c_double
-  use co_all_module, only : co_all
+  use iso_c_binding, only : c_int,c_double,c_bool
+  use co_intrinsics_module, only : co_all,co_product
 #ifdef USE_EXTENSIONS
   use opencoarrays
 #endif
   implicit none               
+  logical :: c_bool_passes=.false.,c_int_passes=.false.
 
 #ifdef USE_EXTENSIONS
   if (this_image()==1) print *,"Using the extensions from the opencoarrays module."
 #endif
 
   ! Verify that every image has a "true" variable with the value .true.
-  verify_co_reduce: block
-    integer(c_int) :: me
-    logical :: true=.true.
-    me=this_image()
+  verify_co_reduce_c_bool: block 
+    logical(c_bool) :: true=.true.
     sync all
     call co_all(true)
-    if (.not.true) then
-      write(error_unit,"(2(a,i2))") "co_min fails for integer(c_int) argument with result (",me,") on image",this_image()
-      error stop
+    if (true) then
+      c_bool_passes=.true.
+    else
+      write(error_unit,"(2(a,i2))") "co_reduce fails for logical(c_bool) argument with result (",true,") on image",this_image()
     end if
-  end block verify_co_reduce
+  end block verify_co_reduce_c_bool
 
-  ! Wait for all images to pass the tests
+  ! Verify the product of image number 
+  verify_co_reduce_c_int: block 
+    integer(c_int) :: me,i
+    me=this_image()
+    sync all
+    call co_product(me)
+    if (me==product(int([(i,i=1,num_images())],c_int))) then
+      c_int_passes=.true.
+    else
+      write(error_unit,"(2(a,i2))") "co_reduce fails integer(c_int) argument with result (",me,") on image",this_image()
+    end if
+  end block verify_co_reduce_c_int
+
+  ! Verify that this image's tests passed
+  if (.not.all([c_bool_passes,c_int_passes])) error stop
+
+  ! Wait for verification that all images to pass the tests
   sync all
   if (this_image()==1) print *, "Test passed."
 end program
