6cfcd1d82621911639d9a59e9575cbb73a7f27f3
Damian Rouson <damian@rouson.net>
2015-11-13 13:00:36 -0800

    Firt complete draft of the installation script.  It has been tested so far on Linux with an acceptable set of pre-installed prerequisites.  It needs to be tested on on OS X and with uninstalled or unacceptable prerequisites to test the the building of the prerequisites. Signed-off-by: Damian Rouson <damian@rouson.net>

--- install.sh
+++ install.sh
@@ -66,90 +66,123 @@ fi
 
 build_path=${PWD}/opencorrays-build
 
-# Interpret the second command-line argument, if present, as the number of threads for 'make'.
-# Otherwise, default to single-threaded 'make'.
-if [ -z $2 ]; then
+#Interpret the second command-line argument, if present, as the number of threads for 'make'.
+#Otherwise, default to single-threaded 'make'.
+if [[ -z $2 ]]; then
   num_threads=1
 else
   num_threads=$2
 fi
 
-CC_DEFAULT=gcc
-FC_DEFAULT=gfortran
-MPICC_DEFAULT=mpicc
-MPIFC_DEFAULT=mpif90
-
-printf "Checking whether CC and FC are both defined..."
-if [[ -z $CC || -z $FC ]]; then
-  CC=$CC_DEFAULT
-  FC=$FC_DEFAULT
-  printf "no.\n"
-  printf "Using default CC=$CC, FC=$FC\n"
-fi
-
-printf "Checking whether MPICC and MPIFC are both defined..."
-if [[ -z $MPICC || -z $MPIF90 ]]; then
-  MPICC=$MPICC_DEFAULT
-  MPIFC=$MPIFC_DEFAULT
-  printf "no.\n"
-  printf "Using default MPICC=$MPICC, MPIFC=$MPIFC\n"
-fi
-
 find_or_install()
 {
-  package_to_check=$1
-  printf "Checking whether $package_to_check is in the PATH... "
-  if type $package_to_check > /dev/null; then
+  package=$1
+  # This is a bash 3 hack standing in for a bash 4 hash (bash 3 is the lowest common
+  # denominator because, for licensing reasons, OS X only has bash 3 by default.)
+  # See http://stackoverflow.com/questions/1494178/how-to-define-hash-tables-in-bash
+  package_executable_array=( 
+    "gcc:gfortran"
+    "cmake:cmake"
+    "mpich:mpif90"
+    "_unknown:0" 
+  )
+  for element in "${package_executable_array[@]}" ; do
+     KEY="${element%%:*}"
+     VALUE="${element##*:}"
+     if [[ "$KEY" == "_unknown" ]]; then
+       # No recognizeable argument passed so print usage information and exit:
+       printf "$this_script: Package name not recognized in function find_or_install.\n"
+       exit 1
+     elif [[ $package == "$KEY" ]]; then
+       executable=$VALUE
+       break
+     fi  
+  done
+  printf "Checking whether $executable is in the PATH... "
+  if type $executable > /dev/null; then
     printf "yes.\n"
-    return
-  fi
-  # Set the MPI Fortran compiler for building OpenCoarrays prerequisites.
-  if [ ! -z $MPIF90 ]; then
 
-    # MPIF90 environment variable is not empty
-    printf "Environment variable MPIF90=$MPIF90.\n"
-    if type $MPIF90 > /dev/null; then
-      # MPIF90 environment variable specifies an executable program
-      printf "Using MPIF90=$MPIF90 as the MPI Fortran compiler.\n"
+    if [[ $package == "mpich" ]]; then
+      echo "Using the pre-installed mpicc and mpif90"
+      MPICC=mpicc
+      MPIFC=mpif90
+    fi
+
+    never=false
+    until [ $never == true ]; do
+    # To avoid an infinite loop every branch must end with return, exit, or break
+
+      if [[ $package != "gcc" ]]; then
+
+        return # return
+
+      else #package is gcc
+
+        printf "Checking whether gfortran is version 5.1.0 or later..."
+        gfortran -o acceptable_compiler ./install_prerequisites/acceptable_compiler.f90
+        gfortran -o print_true ./install_prerequisites/print_true.f90
+        is_true=`./print_true`
+        acceptable=`./acceptable_compiler`
+        rm acceptable_compiler print_true
+  
+        if [[ $acceptable == $is_true ]]; then
+
+          printf "yes\n"
+          FC=gfortran
+          CC=gcc
+          CXX=g++
+
+          return # found an acceptable gfortran in PATH
+
+        else
+          printf "no\n"
+
+          break # need to install gfortran below
+        fi 
+        printf "$this_script: an infinite until loop is missing a break, return, or exit.\n"
+        exit 1 # This should never be reached
+      fi 
+      printf "$this_script: an infinite until loop is missing a break, return, or exit.\n"
+      exit 1 # This should never be reached
+    done
+    # The $executable is not an acceptable version
+  fi 
+
+  # Now we know the $executable is not in the PATH or is not an acceptable version
+  printf "Ok to downloand, build, and install $package from source?\n"
+  if [[ $proceed == "y" ]]; then
+    printf "Downloading, building, and installing $package \n"
+    cd install_prerequisites &&
+    FC=gfortran CC=gcc CXX=g++ ./build $package &&
+    package_install_path=`./build $package --default --query` &&
+    if [[ -f $package_install_path/bin/$executable ]]; then
+      printf "Installation successful."
+      printf "$package is in $package_install_path/bin \n"
+      if [[ $package == "gcc" ]]; then
+        FC=$package_install_path/bin/gfortran
+        CC=$package_install_path/bin/gcc
+        CXX=$package_install_path/bin/g++
+      elif [[ $package == "mpich" ]]; then
+        MPICC=$package_install_path/bin/mpicc
+        MPIFC=$package_install_path/bin/mpif90
+      else
+        PATH=$package_install_path/bin:$PATH
+      fi
+      return
     else
-      # The specified MPIF90 was not found or does not execute. Time to bail.
-      printf "MPIF90=$MPIF90 not found.  Please set the MPIF90 environment variable to the \n"
-      printf "name of an MPI Fortran compiler and prepend the path if it is not in your PATH.\n"
+      printf "Installation unsuccessful."
+      printf "$package is not in the expected path: $package_install_path/bin \n"
       exit 1
     fi
-
-  else # MPIF90 environment variable is empty
-
-    printf "Environment variable MPIF90 is empty. Trying mpif90'.\n"
-
-    if type $MPIF90_DEFAULT > /dev/null; then
-      # mpif90 is an executable program in the user's PATH
-      printf "Found mpif90 in your path. Using mpif90 as the MPI Fortran compiler.\n"
-      MPIF90=$MPIF90_DEFAULT
-
-    else  #
-      printf "No $MPIF90_DEFAULT in your path. Ok to download and install the default MPI (MPICH)? (y/n).\n"
-      read proceed
-      if [[ $proceed == "y" ]]; then
-        printf "Downloading, building, and installing MPICH.\n"
-        cd install_prerequisites &&
-        FC=$FC_DEFAULT CC=$CC_DEFAULT ./build mpich &&
-        mpich_install_path=`./build mpich --default --query` &&
-        if [[ -f "$mpich_install_path/bin/mpif90" ]]; then 
-          printf "Installed $MPI_F90 in $mpich_install_path (You might add the 'bin' subdirectory to your PATH).\n"
-        else
-          printf "Can't find mpif90 in the expected location: $mpich_install_path/bin/mpif90 \n"
-        fi
-      else 
-        printf "Please install an MPI Fortran compiler and restart $this_script.\n"
-        exit 1
-      fi
-    fi
-     
+  else
+    printf "Aborting. OpenCoarrays installation requires $package"
     exit 1
-  fi  
+  fi
+  printf "$this_script: the dependency installation logic is missing a return or exit.\n"
   exit 1
 }
+
+######  Main Body ########## 
 
 if [[ $1 == '--help' || $1 == '-h' ]]; then
   # Print usage information if script is invoked with --help or -h argument
@@ -172,29 +205,36 @@ else
   installation_record=install-opencoarrays.log
   time \
   {
-    find_or_install "cmake"     &&
-    find_or_install "$MPIFC"    &&
-    find_or_install "$MPICC"    &&
+    # Find or install GCC first to ensure we build MPICH with an acceptable version of GCC
+    find_or_install gcc         &&
+    find_or_install mpich       &&
+    find_or_install cmake       &&
     mkdir -p opencoarrays-build &&
     cd opencoarrays-build       &&
-    CC=$MPICC FC=$MPIFC cmake .. -DCMAKE_INSTALL_PREFIX=$install_path &&
-    make         &&
-    make install &&
-    make clean
+    if [[ -z $MPICC || -z $MPIFC ]]; then
+      echo "Empty MPICC=$MPICC or MPIFC=$MPIFC" 
+      exit 1
+    else
+      CC=$MPICC FC=$MPIFC cmake .. -DCMAKE_INSTALL_PREFIX=$install_path &&
+      make         &&
+      make install &&
+      make clean
+    fi
   } >&1 | tee $installation_record
 
   # Report installation success or failure
   printf "\n"
   if [[ -f $install_path/bin/caf && -f $install_path/bin/cafrun  && -f $install_path/bin/build ]]; then
-    printf "$this_script: Done.\n"
+    printf "$this_script: Done.\n\n"
     printf "The OpenCoarrays compiler wrapper (caf), program launcher (cafrun), and\n"
     printf "prerequisite package installer (build) are in the following directory:\n"
-    printf "$install_path/bin.\n"
+    printf "$install_path/bin.\n\n"
   else
+    printf "$this_script: Done. \n\n"
     printf "Something went wrong. The OpenCoarrays compiler wrapper (caf),\n" 
     printf "program launcher (cafrun), and prerequisite package installer (build) \n"
     printf "are not in the expected location:\n"
     printf "$install_path/bin.\n"
-    printf "Please review the '$installation_record' file for more information.\n"
+    printf "Please review the '$installation_record' file for more information.\n\n"
   fi
 fi
