9c5e51a42ac544eddcfdea5b1cd4b884e7e75d0f
Damian Rouson <damian@sourceryinstitute.org>
2016-06-01 23:13:01 +0000

    Removed download-mechanism platform-dependence.
    Use svn for all branch builds or branch list queries
    Use curl, wget, or ftp in decreasing order of preference as measured by robustness.

--- prerequisites/build-functions/set_or_print_downloader.sh
+++ prerequisites/build-functions/set_or_print_downloader.sh
@@ -4,7 +4,6 @@
 # shellcheck disable=SC2154
 set_or_print_downloader()
 {
-
   # Verify requirements
   [ ! -z "${arg_D}" ] && [ ! -z "${arg_p:-${arg_P:-${arg_U:-${arg_V:-${arg_B}}}}}" ] &&
     emergency "Please pass only one of {-B, -D, -p, -P, -U, -V} or a longer equivalent (multiple detected)."
@@ -14,48 +13,42 @@ set_or_print_downloader()
   if [[ "${package_name}" == "ofp" ]]; then
     ${OPENCOARRAYS_SRC_DIR}/prerequisites/install-ofp.sh "${@}"
     exit 0
-  fi
-  if [[ $(uname) == "Darwin" ]]; then
-    wget_or_curl=curl
-    ftp_or_curl=curl
+  fi  
+
+  # Choose the first available download mechanism, prioritizing first any absolute requirement 
+  # (svn for gcc development branches) and second robustness:
+  info "Checking available download mechanisms: ftp, wget, and curl."
+  info "\${package_name}=${package_name}  \${arg_b:-\${arg_B:-}}=${arg_b:-${arg_B:-}}"
+   
+  if [[ "${package_name}" == "gcc" && ! -z "${arg_b:-${arg_B:-}}" ]]; then
+
+    if type svn &> /dev/null; then
+      fetch=svn 
+    else 
+      tried="svn"
+    fi
+
+  elif type curl &> /dev/null; then
+    fetch=curl
+  elif type ftp &> /dev/null; then
+    fetch=wget
+  elif type wget &> /dev/null; then
+    fetch=ftp-url
   else
-    wget_or_curl=wget
-    ftp_or_curl=ftp-url
-  fi
-  if [[ "${package_name}" == "gcc" ]]; then
-    if [[ -z "${arg_b:-${arg_B:-}}" ]]; then
-      gcc_fetch="${ftp_or_curl}"
-    else
-      gcc_fetch="svn"
+    tried="curl, wget, and ftp"
+  fi  
+
+  if [[ -z "${fetch:-}" ]]; then
+    if [[ -z "${arg_B:-}" ]]; then
+      warning "No available download mechanism. Options tried: ${tried}" 
+    else 
+      emergency "No available download mechanism. Option tried: ${tried}" 
     fi
   fi
-  # This is a bash 3 hack standing in for a bash 4 hash (bash 3 is the lowest common
-  # denominator because, for licensing reasons, OS X only has bash 3 by default.)
-  # See http://stackoverflow.com/questions/1494178/how-to-define-hash-tables-in-bash
-  package_fetch=(
-    "gcc:${gcc_fetch-}"
-    "wget:${ftp_or_curl-}"
-    "cmake:${wget_or_curl-}"
-    "mpich:${wget_or_curl-}"
-    "flex:${wget_or_curl-}"
-    "bison:${ftp_or_curl-}"
-    "pkg-config:${wget_or_curl-}"
-    "make:${ftp_or_curl-}"
-    "m4:${ftp_or_curl-}"
-    "subversion:${wget_or_curl-}"
-  )
-  for package in "${package_fetch[@]}" ; do
-     KEY="${package%%:*}"
-     VALUE="${package##*:}"
-     if [[ "${package_name}" == "${KEY}" ]]; then
-       # We recognize the package name so we set the download mechanism:
-       fetch=${VALUE}
-       # If a printout of the download mechanism was requested, then print it and exit with normal status
-       [[ ! -z "${arg_D}" ]] && printf "%s\n" "${fetch}" && exit 0
-       break # exit the for loop
-     fi
-  done
-  if [[ -z "${fetch:-}" ]]; then
-    emergency "Package ${package_name:-} not recognized.  Use --l or --list-packages to list the allowable names."
+
+  # If a printout of the download mechanism was requested, then print it and exit with normal status
+  if [[ ! -z "${arg_D}" ]]; then 
+     printf "%s\n" "${fetch}" 
+     exit 0
   fi
 }
