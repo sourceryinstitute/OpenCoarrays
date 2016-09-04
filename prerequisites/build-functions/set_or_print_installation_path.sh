b842712cc3b4a45b4d95dabbabf6101ad05c9567
Damian Rouson <damian@sourceryinstitute.org>
2016-09-04 12:17:57 -0700

    Treat user-specified install prefix as full path (#213)
    * Treat user-specified install prefix as full path
    
    previously subdirectories named after the package and version were
    appended to user-specified install prefixes so
    
    ./install.sh -p gcc -i /opt -I 6.1.0
    
    would produce an installation directory named "/opt/gcc/6.1.0".
    This differs from how most packages interpret the install prefix
    and can result in redundancy if the user has already included such
    information in the -i argument. With this commmit, the bare user-
    -specified install prefix will be used with nothing appended.  If
    no -i argument is present, the default still contains the package
    name and version: ${OPENCOARRAYS_SRC_DIR}/prerequisites/installations/gcc/6.1.0)
    
    * Update MPICH-GCC bottle for Travis-CI testing
    
     See
     https://github.com/sourceryinstitute/opencoarrays/issues/65#issuecomment-244381280
     for details
    (cherry picked from commit 0dded257c7842bb7a6d54b83134b23a887c2056d)

--- prerequisites/build-functions/set_or_print_installation_path.sh
+++ prerequisites/build-functions/set_or_print_installation_path.sh
@@ -7,7 +7,12 @@ set_or_print_installation_path()
   [ ! -z "${arg_P}" ] && [ ! -z "${arg_p:-${arg_D:-${arg_U:-${arg_V}}}}" ] &&
     emergency "Please pass only one of {-D, -p, -P, -U, -V} or a longer equivalent (multiple detected)."
 
-  install_path="${arg_i%/}/${arg_p:-${arg_D:-${arg_P:-${arg_U:-${arg_V}}}}}/${version_to_build}"
+  arg_i_default="${OPENCOARRAYS_SRC_DIR}/prerequisites/installations/"
+  if [[ "${arg_i:-}" ==  "${arg_i_default}" ]]; then
+    install_path="${arg_i%/}/${arg_p:-${arg_D:-${arg_P:-${arg_U:-${arg_V}}}}}/${version_to_build}"
+  else
+    install_path="${arg_i%/}"
+  fi
 
   # If -P is present, print ${install_path} and exit with normal status
   if [[ ! -z "${arg_P:-}" ]]; then
