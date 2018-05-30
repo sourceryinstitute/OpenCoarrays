23c5d3c3ec725728e97ac10527a0b512242f3250
Izaak Beekman <zbeekman@gmail.com>
2018-05-30 12:21:16 -0400

    Fix install.linux.sh travis_wait issue

--- developer-scripts/travis/install.linux.sh
+++ developer-scripts/travis/install.linux.sh
@@ -31,15 +31,16 @@ if [[ "${BUILD_TYPE:-}" != InstallScript ]]; then # Ubuntu on Travis-CI, NOT tes
         tar -xzvf "${MPICH_URL_TAIL}" > tar_mpich.log 2>&1 || cat tar_mpich.log
         export CC=gcc-${GCC}
         export FC=gfortran-${GCC}
-        cd "${MPICH_URL_TAIL%.tar.gz}" || exit 4
-        echo "Configuring MPICH ..."
-        type -a travis_wait
-        ${TRAVIS:+travis_wait} ./configure --prefix="${CACHE}" > configure_mpich.log 2>&1 || cat configure_mpich.log
-        echo "Building MPICH ..."
-        ${TRAVIS:+travis_wait 30} make -j 4 > make_mpich.log 2>&1 || cat make_mpich.log
-        echo "Installing MPICH ..."
-        ${TRAVIS:+travis_wait} make install > install_mpich.log 2>&1 || cat install_mpich.log
-        cd - || exit 4
+        (
+	    cd "${MPICH_URL_TAIL%.tar.gz}" || exit 4
+            echo "Configuring MPICH ..."
+	    pwd
+            ./configure --prefix="${CACHE}"
+            echo "Building MPICH ..."
+            make -j 4
+            echo "Installing MPICH ..."
+            make install
+	)
     fi
 fi
 
