77182e2c9480aa746bac406f273361d36c358b3c
Damian Rouson <damian@sourceryinstitute.org>
2018-11-20 07:26:40 -0800

    Shorten teams_subset test; refactor to use assert

--- src/tests/unit/teams/teams_subset.f90
+++ src/tests/unit/teams/teams_subset.f90
@@ -1,35 +1,36 @@
 program teams_subset
+  !! author: Nathan Weeks
+  !!
+  !! Regression test for prior potential deadlock in change team/end team
   use iso_fortran_env, only : team_type
+  use assertions_interface, only : assert
   implicit none
 
-  type(team_type) :: team
-  integer :: initial_team_image, max_image(2), min_image(2), myteam
+  type(team_type) team
 
-  initial_team_image = this_image()
+  associate( initial_team_image => this_image())
+  associate( myteam => merge(1,2,any(initial_team_image==[1,num_images()]) )
 
-  if (initial_team_image == 1 .or. initial_team_image == num_images()) then
-    myteam = 1
-  else
-    myteam = 2
-  end if
+    form team (myteam, team)
 
-  form team (myteam, team)
-
-  if (myteam == 1) then
-    change team(team)
-      max_image = [initial_team_image, this_image()]
-      call co_max(max_image)
-      min_image = [initial_team_image, this_image()]
-      call co_min(min_image)
-    end team
-    if (any(min_image /= [1, 1]) .or. any(max_image /= [num_images(), 2])) then
-      write(*,*) "Test failed."
-      error stop
+    if (myteam == 1) then
+      block
+        integer max_image(2), min_image(2)
+        change team(team)
+          max_image = [initial_team_image, this_image()]
+          call co_max(max_image)
+          min_image = [initial_team_image, this_image()]
+          call co_min(min_image)
+        end team
+        call assert( all(min_image==1), "team minimum image number is 1" )
+        call assert( all(max_image==[num_images(), 2]), "team maximum images numbers is num_images() or 2")
+      end block
     end if
-  end if
 
-  sync all
+    sync all
 
-  if (initial_team_image == 1) write(*,*) "Test passed."
+    if (initial_team_image == 1) print *,"Test passed."
+
+  end associate; end associate
 
 end program
