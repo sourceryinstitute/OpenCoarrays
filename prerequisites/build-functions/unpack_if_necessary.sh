fce0a355e7d92d44067f3ed320e8bf0c1993af46
Damian Rouson <damian@sourceryinstitute.org>
2018-12-06 16:34:14 -0800

    Fix file-extension check for conditional unpacking

--- prerequisites/build-functions/unpack_if_necessary.sh
+++ prerequisites/build-functions/unpack_if_necessary.sh
@@ -5,7 +5,7 @@ unpack_if_necessary()
   if [[ "${fetch}" == "svn" || "${fetch}" == "git" ]]; then
     package_source_directory="${version_to_build}"
   else
-    if [[ ${url_tail} == "*tar.gz" || ${url_tail} == "*tar.bz2" || ${url_tail} == "*.tar.xz" ]]; then
+    if [[ "${url_tail}" == *tar.gz || "${url_tail}" == *tar.bz2 || "${url_tail}" == *.tar.xz ]]; then
       info "Unpacking ${url_tail}."
       info "pushd ${download_path}"
       pushd "${download_path}"
@@ -14,7 +14,7 @@ unpack_if_necessary()
       info "popd"
       popd
     else
-      info "Skipping unpacking because ${url_tail} is not a compressed archive (*.gz, *.bz2, or *.xz). "
+      info "Skipping unpacking because ${url_tail} is not a compressed archive (matching one of *.tar.{gz,bz2,xz}). "
     fi
     # shellcheck disable=SC2034
     package_source_directory="${package_name}-${version_to_build}"
