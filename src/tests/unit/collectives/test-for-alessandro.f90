af7969b32444d14ac3e91ef87489d26ce49dd2f1
Damian Rouson <damian@sourceryinstitute.org>
2019-05-14 15:50:58 -0700

    Demonstrate alloc component co_broadcast failure

---
+++ src/tests/unit/collectives/test-for-alessandro.f90
@@ -0,0 +1,22 @@
+program main
+  !! This test seg faults for if num_images() >  1
+  implicit none
+
+  type dynamic
+    integer, allocatable :: vector(:)
+  end type
+  type(dynamic) alloc_message, alloc_content
+   
+  integer, parameter :: sender=1 !! co_broadcast source_image
+
+  alloc_content%vector = [1,2,3,4]
+
+  if (this_image()==sender) alloc_message = alloc_content
+
+  call co_broadcast(alloc_message,source_image=sender)
+  if ( any(alloc_message%vector /= alloc_content%vector) ) error stop "Test failed."
+
+  sync all  ! Wait for each image to pass the test
+  if (this_image()==sender) print *,"Test passed."
+
+end program main
