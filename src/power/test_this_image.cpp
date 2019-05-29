841067fe8ba1ba3f191fc1e0a24b225df254c786
Katherine Rasmussen <ktrasmussn@gmail.com>
2019-05-29 12:27:33 -0700

    Add test for this_image()

---
+++ src/power/test_this_image.cpp
@@ -0,0 +1,19 @@
+// Test this_image()
+
+#include <cassert>
+#include "libcaf.h"
+
+int main(int argc, char* argv[])
+{
+   caf_init(&argc, &argv);        // initialize image
+
+   int num = num_images();        // get number of images
+   assert(num > 0);
+
+   int img = this_image();        // get this image
+   assert(img > 0 && img <= num); // assert this image is within range of number of images
+
+   caf_finalize();                // finalize image
+
+   return 0;
+}
