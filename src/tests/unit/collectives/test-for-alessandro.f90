523b45e48408ba877d7281fecd574cfdfe729398
Alessandro Fanfarillo <alessandro.fanfarillo@gmail.com>
2019-07-02 21:12:11 -0600

    Allocate destination array in simple testcase

--- src/tests/unit/collectives/test-for-alessandro.f90
+++ src/tests/unit/collectives/test-for-alessandro.f90
@@ -11,7 +11,13 @@ program main
 
   alloc_content%vector = [1,2,3,4]
 
-  if (this_image()==sender) alloc_message = alloc_content
+  if (this_image()==sender) then
+     alloc_message = alloc_content
+  else
+     allocate(alloc_message%vector(4))
+  end if
+
+  sync all
 
   call co_broadcast(alloc_message,source_image=sender)
   if ( any(alloc_message%vector /= alloc_content%vector) ) error stop "Test failed."
