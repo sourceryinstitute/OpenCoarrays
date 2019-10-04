0c55821fb0d2d1ba64afc2cb27c21999ddf3f586
Damian Rouson <damian@sourceryinstitute.org>
2019-10-03 18:28:44 -0700

    adjust co_broadcast_derived... test for GCC < 8

--- src/tests/unit/collectives/co_broadcast_derived_type.f90
+++ src/tests/unit/collectives/co_broadcast_derived_type.f90
@@ -24,29 +24,29 @@ program main
   type, extends(parent) :: child
 
     ! Scalar and array derived-type components
-    type(component) a, b(1,2,1, 1,1,1, 1,1,1, 1,1,1, 1,1,1)
+    type(component) a, b(1,2,1, 1,1,1, 1)
 
     ! Scalar and array intrinsic-type components
     character(len=len(text)) :: c="", z(0)
     complex :: i=(0.,0.), j(1)=(0.,0.)
     integer :: k=0,       l(2,3)=0
-    logical :: r=.false., s(1,2,3, 1,2,3, 1,2,3, 1,2,3, 1,2,3)=.false.
+    logical :: r=.false., s(1,2,3, 1,2,3, 1)=.false.
     real    :: t=0.,      u(3,2,1)=0.
 
     ! Scalar and array pointer components
     character(len=len(text)), pointer :: &
-                        char_ptr=>null(), char_ptr_maxdim(:,:,:, :,:,:, :,:,:, :,:,:, :,:,:)=>null()
-    complex, pointer :: cplx_ptr=>null(), cplx_ptr_maxdim(:,:,:, :,:,:, :,:,:, :,:,:, :,:,:)=>null()
-    integer, pointer :: int_ptr =>null(), int_ptr_maxdim (:,:,:, :,:,:, :,:,:, :,:,:, :,:,:)=>null()
-    logical, pointer :: bool_ptr=>null(), bool_ptr_maxdim(:,:,:, :,:,:, :,:,:, :,:,:, :,:,:)=>null()
-    real, pointer    :: real_ptr=>null(), real_ptr_maxdim(:,:,:, :,:,:, :,:,:, :,:,:, :,:,:)=>null()
+                        char_ptr=>null(), char_ptr_maxdim(:,:,:, :,:,:, :)=>null()
+    complex, pointer :: cplx_ptr=>null(), cplx_ptr_maxdim(:,:,:, :,:,:, :)=>null()
+    integer, pointer :: int_ptr =>null(), int_ptr_maxdim (:,:,:, :,:,:, :)=>null()
+    logical, pointer :: bool_ptr=>null(), bool_ptr_maxdim(:,:,:, :,:,:, :)=>null()
+    real, pointer    :: real_ptr=>null(), real_ptr_maxdim(:,:,:, :,:,:, :)=>null()
     procedure(f), pointer :: procedure_pointer=>null()
   end type
 
   type(child) message
   type(child) :: content = child( & ! define content using the insrinsic structure constructor
     parent=parent(heritable=-4),                                                                           & ! parent
-    a=component(-3), b=reshape([component(-2),component(-1)], [1,2,1, 1,1,1, 1,1,1, 1,1,1, 1,1,1]),        & ! derived types
+    a=component(-3), b=reshape([component(-2),component(-1)], [1,2,1, 1,1,1, 1]),        & ! derived types
     c=text, z=[character(len=len(text))::], i=(0.,1.), j=(2.,3.), k=4, l=5, r=.true., s=.true., t=7., u=8. & ! intrinsic types
   )
 
@@ -54,11 +54,11 @@ program main
 
     if (me==sender) then
       message = content
-      allocate(message%char_ptr, message%char_ptr_maxdim(1,1,2, 1,1,1, 1,1,1, 1,1,1, 1,1,1), source=text   )
-      allocate(message%cplx_ptr, message%cplx_ptr_maxdim(1,1,1, 1,1,2, 1,1,1, 1,1,1, 1,1,1), source=(0.,1.))
-      allocate(message%int_ptr , message%int_ptr_maxdim (1,1,1, 1,1,1, 1,1,2, 1,1,1, 1,1,1), source=2      )
-      allocate(message%bool_ptr, message%bool_ptr_maxdim(1,1,1, 1,2,1, 1,1,1, 1,1,2, 1,1,1), source=.true. )
-      allocate(message%real_ptr, message%real_ptr_maxdim(1,1,1, 1,1,1, 1,1,1, 1,1,1, 1,1,2), source=3.     )
+      allocate(message%char_ptr, message%char_ptr_maxdim(1,1,2, 1,1,1, 1), source=text   )
+      allocate(message%cplx_ptr, message%cplx_ptr_maxdim(1,1,1, 1,1,2, 1), source=(0.,1.))
+      allocate(message%int_ptr , message%int_ptr_maxdim (1,1,1, 1,1,1, 1), source=2      )
+      allocate(message%bool_ptr, message%bool_ptr_maxdim(1,1,1, 1,2,1, 1), source=.true. )
+      allocate(message%real_ptr, message%real_ptr_maxdim(1,1,1, 1,1,1, 1), source=3.     )
     end if
 
     call co_broadcast(message,source_image=sender)
