abf9b5c27a9a568f8fd9a60b3f13a3c546be6288
Damian Rouson <damian@sourceryinstitute.org>
2020-02-24 02:00:25 -0800

    fix package path

--- prerequisites/build-functions/unpack_if_necessary.sh
+++ prerequisites/build-functions/unpack_if_necessary.sh
@@ -3,7 +3,7 @@
 unpack_if_necessary()
 {
   if [[ "${fetch}" == "git" ]]; then
-    package_source_directory="${version_to_build}"
+    package_source_directory="${package_name}"
   else
     if [[ "${url_tail}" == *tar.gz || "${url_tail}" == *tar.bz2 || "${url_tail}" == *.tar.xz ]]; then
       info "Unpacking ${url_tail}."
