1935b64e3e0d1e47cd42de59166f78570a7e2c40
Damian Rouson <rouson@lbl.gov>
2022-04-24 12:44:55 -0700

    feat(fpm): run-fpm.sh accepts arguments
    This commit eliminates the hardwired, separate build and run steps
    in run-fpm.sh and instead passes an command-line arguments along
    to fpm.  The following now works even with no prior build directory
    present:
    
    ./run-fpm.sh --example hello

--- run-fpm.sh
+++ run-fpm.sh
@@ -4,18 +4,9 @@ if ! command -v mpifort > /dev/null 2>&1; then
   brew install open-mpi
 fi
 
-MPIFC=`which mpifort`
-MPICC=`which mpicc`
-
-fpm build \
-  --compiler $MPIFC \
-  --c-compiler $MPICC \
-  --c-flag "-DPREFIX_NAME=_gfortran_caf_ -DGCC_GE_7 -DGCC_GE_8" \
-  --flag "-fcoarray=lib"
-
-fpm run --example \
-  --compiler $MPIFC \
-  --c-compiler $MPICC \
+fpm $@ \
+  --compiler `which mpifort` \
+  --c-compiler `which mpicc` \
   --c-flag "-DPREFIX_NAME=_gfortran_caf_ -DGCC_GE_7 -DGCC_GE_8" \
   --flag "-fcoarray=lib" \
   --runner "mpirun -n 4"
